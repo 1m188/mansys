634959ab78a01585a4ae7cc10f41ace70502194f
1m188 <228117330@qq.com>
2018-04-06 12:08:57 +0800

    主流程框架

--- 学生信息管理系统/main.py
+++ 学生信息管理系统/main.py
@@ -1,11 +1,19 @@
-﻿from PyQt5.QtWidgets import QApplication,QWidget
+﻿from PyQt5.QtWidgets import QApplication,QDialog
 import sys
+from data import Data
 from Gui.loginGui import LoginGui
+from Gui.mainGui import MainGui
 
 
 app = QApplication(sys.argv)
 
-loginGui = LoginGui()
-loginGui.show()
+data = Data()
 
-app.exec()
+loginGui = LoginGui()
+data.addClass(loginGui)
+loginGui.show()
+if loginGui.exec() == QDialog.Accepted:
+    mainGui = MainGui()
+    data.addClass(mainGui)
+    mainGui.show()
+    app.exec()
