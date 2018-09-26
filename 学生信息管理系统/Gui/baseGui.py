3e9abe864e69c095e44630d471e0e4d771bf959d
1m188 <228117330@qq.com>
2018-09-26 23:53:55 +0800

    基本界面类完善了注释，修改了pyqt5模块的import

--- 学生信息管理系统/Gui/baseGui.py
+++ 学生信息管理系统/Gui/baseGui.py
@@ -2,15 +2,18 @@
 一个基本的界面类，包含了消息信号和消息信号槽，这样其他的界面只需继承这个类即可获得相应的信号和信号槽
 '''
 
-from PyQt5.Qt import *
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QMessageBox
 from msg import Msg
 
 
 class BaseGui:
-    msgSignal = pyqtSignal(Msg)
+    msgSignal = pyqtSignal(Msg) #消息通知信号
 
+    #接收到消息通知信号槽
     def msgSlot(self,msg):
-        if msg.aim == type(self):
-            QMessageBox.about(self,msg.title,msg.msg)
+        if msg.aim == type(self): #如果消息的目标是自己的类型
+            QMessageBox.about(self,msg.title,msg.msg) #则通知
         else:
+            #否则继续发送（如果和其他的界面有联系的话）
             self.msgSignal.emit(msg)
