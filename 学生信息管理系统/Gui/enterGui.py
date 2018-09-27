ea0e431ea9ef8829b24eb98358c3eec4d53bb2cc
1m188 <228117330@qq.com>
2018-09-27 17:23:01 +0800

    导入界面的代码规范和注释完善，把某些print出来的问题都用Message Box做了。

--- 学生信息管理系统/Gui/enterGui.py
+++ 学生信息管理系统/Gui/enterGui.py
@@ -2,30 +2,36 @@
 一个用来导入学生信息的界面
 '''
 
-from PyQt5.Qt import *
+from PyQt5.QtWidgets import QWidget,QApplication,QLabel,QLineEdit,QPushButton,QGridLayout,QMessageBox
+from PyQt5.QtCore import pyqtSignal,Qt,QRegExp
+from PyQt5.QtGui import QFont,QRegExpValidator
 from Gui.baseGui import BaseGui
 
 
 class EnterGui(QWidget,BaseGui):
-    enterSignal = pyqtSignal(str)
-    closeSignal = pyqtSignal()
+    enterSignal = pyqtSignal(str) #导入请求
+    closeSignal = pyqtSignal() #关闭信号
 
     def __init__(self):
         super().__init__()
 
+        #界面基本设置
         self.setAttribute(Qt.WA_QuitOnClose,False)
         self.setAttribute(Qt.WA_DeleteOnClose,True)
 
-        self.initUI()
-
-    def initUI(self):
+        #设置界面标题和大小
         self.setWindowTitle("导入学生信息")
         self.resize(500,200)
 
+        #移动到屏幕中央
         rect = self.frameGeometry()
         rect.moveCenter(QApplication.desktop().availableGeometry().center())
         self.move(rect.topLeft())
 
+        self.initUI() #UI设置
+
+    def initUI(self):
+        #控件
         infoLabel = QLabel(self)
         infoLabel.setAlignment(Qt.AlignCenter)
         infoLabel.setFont(QFont("微软雅黑",15))
@@ -65,6 +71,7 @@ class EnterGui(QWidget,BaseGui):
         enterButton.setText("导入")
         enterButton.clicked.connect(self.enterButtonClicked)
 
+        #布局
         layout = QGridLayout(self)
         layout.addWidget(infoLabel,0,0,2,12)
         layout.addWidget(nameLabel,2,0,1,3)
@@ -77,12 +84,14 @@ class EnterGui(QWidget,BaseGui):
         layout.addWidget(self.professLineEdit,3,9,1,3)
         layout.addWidget(enterButton,4,11,1,1)
 
+    #单击导入按钮
     def enterButtonClicked(self):
         if self.nameLineEdit.text() == "" or self.ageLineEdit.text() == "" or self.numLineEdit.text() == "" or self.professLineEdit.text() == "":
-            print("学生信息不可为空！")
+            QMessageBox.critical(self,"错误","学生信息不可为空！",QMessageBox.Ok)
         else:
             self.enterSignal.emit(self.nameLineEdit.text() + ' ' + self.ageLineEdit.text() + ' ' + self.numLineEdit.text() + ' ' + self.professLineEdit.text())
 
+    #界面关闭的时候发出这个信号表示界面已经关闭
     def closeEvent(self, QCloseEvent):
         self.closeSignal.emit()
         return super().closeEvent(QCloseEvent)
