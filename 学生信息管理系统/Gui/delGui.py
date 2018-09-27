9e8eb76353508d93d6d11d44f78ed2bcdde37e12
1m188 <228117330@qq.com>
2018-09-27 17:29:31 +0800

    删除界面的代码规范和完善注释，把问题的print都变成了MessageBox。

--- 学生信息管理系统/Gui/delGui.py
+++ 学生信息管理系统/Gui/delGui.py
@@ -2,30 +2,36 @@
 用来删除学生信息的界面
 '''
 
-from PyQt5.Qt import *
+from PyQt5.QtWidgets import QWidget,QApplication,QLabel,QLineEdit,QPushButton,QGridLayout,QMessageBox
+from PyQt5.QtCore import pyqtSignal,Qt
+from PyQt5.QtGui import QFont
 from Gui.baseGui import BaseGui
 
 
 class DelGui(QWidget,BaseGui):
-    delSignal = pyqtSignal(str)
-    closeSignal = pyqtSignal()
+    delSignal = pyqtSignal(str) #删除请求
+    closeSignal = pyqtSignal() #界面关闭信号
 
     def __init__(self):
         super().__init__()
 
+        #界面基本设置
         self.setAttribute(Qt.WA_QuitOnClose,False)
         self.setAttribute(Qt.WA_DeleteOnClose,True)
 
-        self.initUI()
-
-    def initUI(self):
+        #设置界面标题和大小
         self.setWindowTitle("删除学生信息")
         self.resize(500,200)
 
+        #移动到屏幕中央
         rect = self.frameGeometry()
         rect.moveCenter(QApplication.desktop().availableGeometry().center())
         self.move(rect.topLeft())
 
+        self.initUI() #UI设置
+
+    def initUI(self):
+        #控件
         infoLabel = QLabel(self)
         infoLabel.setAlignment(Qt.AlignCenter)
         infoLabel.setFont(QFont("微软雅黑",15))
@@ -63,6 +69,7 @@ class DelGui(QWidget,BaseGui):
         delButton.setText("删除")
         delButton.clicked.connect(self.delButtonClicked)
 
+        #布局
         layout = QGridLayout(self)
         layout.addWidget(infoLabel,0,0,2,12)
         layout.addWidget(nameLabel,2,0,1,3)
@@ -75,12 +82,14 @@ class DelGui(QWidget,BaseGui):
         layout.addWidget(self.professLineEdit,3,9,1,3)
         layout.addWidget(delButton,4,11,1,1)
 
+    #单击删除按钮
     def delButtonClicked(self):
         if self.nameLineEdit.text() == "" or self.ageLineEdit.text() == "" or self.numLineEdit.text() == "" or self.professLineEdit.text() == "":
-            print("学生信息不可为空！")
+            QMessageBox.critical(self,"错误","学生信息不可为空！",QMessageBox.Ok)
         else:
             self.delSignal.emit(self.nameLineEdit.text() + ' ' + self.ageLineEdit.text() + ' ' + self.numLineEdit.text() + ' ' + self.professLineEdit.text())
 
+    #关闭的时候发出这个信号，表示界面已经关闭
     def closeEvent(self, QCloseEvent):
         self.closeSignal.emit()
         return super().closeEvent(QCloseEvent)
