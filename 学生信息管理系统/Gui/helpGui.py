e12e507f76e9f99c9d44393b7effe48756f31f98
1m188 <228117330@qq.com>
2018-09-27 17:14:27 +0800

    帮助界面的代码规范和完善注释。

--- 学生信息管理系统/Gui/helpGui.py
+++ 学生信息管理系统/Gui/helpGui.py
@@ -2,26 +2,33 @@
 一个帮助界面
 '''
 
-from PyQt5.Qt import *
+from PyQt5.QtWidgets import QDialog,QLabel,QPushButton,QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
 
 class HelpGui(QDialog):
     def __init__(self):
         super().__init__()
 
+        #界面基本设置
         self.setAttribute(Qt.WA_DeleteOnClose,True)
-
-        self.initUI()
-
-    def initUI(self):
+        self.setAttribute(Qt.WA_QuitOnClose,False)
         self.setWindowFlags(Qt.WindowCloseButtonHint)
+
+        #界面标题和大小
         self.setWindowTitle("帮助")
         self.setFixedSize(600,300)
 
+        #移动到屏幕中央
         rect = self.frameGeometry()
         rect.moveCenter(QApplication.desktop().availableGeometry().center())
         self.move(rect.topLeft())
 
+        self.initUI() #UI启动
+
+    def initUI(self):
+        #控件+布局
         infoLabel = QLabel(self)
         infoLabel.setFont(QFont("微软雅黑",15))
         infoLabel.setText("学生信息管理系统\n\t——相信你一定会用")
