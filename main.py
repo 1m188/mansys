18d4b7903a9e82185a83b8792f4758011701f275
1m188 <228117330@qq.com>
2019-10-18 02:13:47 +0800

    去掉插件加载目录设置的一句

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 整个应用的主流程，启动文件，主要是最开始实例化数据处理类，然后登陆，成功了就进入主界面，addClass函数是用来连接信号和信号槽的
 '''
 
-from PyQt5.QtWidgets import QApplication,QDialog
+from PyQt5.QtWidgets import QApplication, QDialog
 import sys
 from data import Data
 from Gui.loginGui import LoginGui
 from Gui.mainGui import MainGui
-
-QApplication.addLibraryPath(r"env\Lib\site-packages\PyQt5\Qt\plugins")
 
 app = QApplication(sys.argv)
 
